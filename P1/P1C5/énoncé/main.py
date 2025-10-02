a1f5f9e8522151febe5ce221f014cbf005f9c878
alyciabltch524-cyber <alyciabltch524@gmail.com>
2025-10-02 22:31:28 +0200

    Update main.py
    exo 2

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+nom = "ALY"
+age = 21
+taille = 1.52
+est_etudiant = true
+print(f"je m'appelle {nom} , j'ai {age} ,je fais {taille} et je suis etudiante {est_etudiant} ")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
