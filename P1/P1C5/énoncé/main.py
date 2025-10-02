b4e960018652bc9e0045f20c94834784ecb56cdb
alyciabltch524-cyber <alyciabltch524@gmail.com>
2025-10-02 22:35:02 +0200

    Update main.py
    exo 2

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -3,8 +3,11 @@ nom = "ALY"
 age = 21
 taille = 1.52
 est_etudiant = true
-print(f"je m'appelle {nom} , j'ai {age} ,je fais {taille} et je suis etudiante {est_etudiant} ")
-type(nom)
-type(age)
-type(taille)
-type(est_etudiant)
+
+print(f"je m'appelle {nom} , j'ai {age} ,je fais {taille}")
+print(f" est_etudiant : {est_etudiant} ")
+
+print(f'le nom est de : type(nom) ")
+print(f'l'age est de : type(age) ")
+print(f'la taille est de : type(taillle) ")
+print(f'la personne est etudiante : type(est_etudiant) ")
