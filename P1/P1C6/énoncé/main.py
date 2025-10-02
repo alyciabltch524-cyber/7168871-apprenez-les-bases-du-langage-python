76953acb1dd1e5eb6fcbb4daea6e2e6550868624
alyciabltch524-cyber <alyciabltch524@gmail.com>
2025-10-02 23:47:10 +0200

    Update main.py
    exo 3 les listes

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
 # Écrivez votre code ici !
+fruits = ["pomme" , "banane" , "orange" ]
+fruits.append("kiwi")
+
+print(f"dans ma liste de fruits il y a : {fruits}")
+
+fruits.remove("orange")
+print(f" a present il n y a que {fruits} dans ma liste de fruits")
+
+fruits[1] = "ananas"
+print(f"ma liste de fruits se compose de {fruits}")
+
+print(f"la longueur de ma liste de fruits est de {len(fruits)}")
+
+fruits.sort()
+print(f"ma liste de fruits est composé de : {fruits} ")
